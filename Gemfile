d6ea6f5da7fbb52d70a5265f04e9cc147e75da9a
Jorge <jorge@jorge-XPS-8500.(none)>
2012-09-10 15:15:05 -0600

    Modifiaciones a las validaciones de empleados y se agrega gema timeliness

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.3'
 
 gem 'pg'
 gem 'authlogic'
+gem 'validates_timeliness', '~> 3.0'
 
 
 # Gems used only for assets and not required
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,10 +1,6 @@
 class Employee < ActiveRecord::Base
   attr_accessible :name, :address, :hire_date, :horary, :job_title, :salary, :termination_date
-
-  validate :name, :presence => true
-  validate :address, :address => true
-  validate :hire_date, :presence => true
-  validate :salary, :presence => true
-
+  validates_presence_of :name ,:address,:salary,:job_title,:horary
+  validates :hire_date, :timeliness => {:on_or_before => lambda { Date.current }, :type => :date}
 
 end
