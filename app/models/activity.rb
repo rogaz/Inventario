506faa08349b25193a46aa2ef74b2927b17dbf20
Jorge <jorge@jorge-XPS-8500.(none)>
2012-09-13 13:37:51 -0600

    se agregan validaciones a actividades y a clientes

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,4 @@
 class Activity < ActiveRecord::Base
   attr_accessible :date, :description, :title
+  validates_presence_of :description, :title, :date
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ActiveRecord::Base
   attr_accessible :last_visit_date, :name, :phone, :reference_person
+  validates_presence_of :last_visit_date , :name, :phone, :reference_person
 end
