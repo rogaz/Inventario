8eadcb1432f70d9608fd9a38cb049f7682229ad8
Raul Ogaz <raul.26_90@hotmail.com>
2012-09-16 17:08:16 -0600

    Se valida en gastos que la fecha no sea mayor a la actual y se le agrega datepicker

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,8 @@
 class Expense < ActiveRecord::Base
   attr_accessible :date, :description, :total
+
+  validates :date, :presence => true
+  validates :description, :presence => true
+  validates :total, :presence => true
+  validates :date, :timeliness => {:on_or_before => lambda { Date.current }, :type => :date}
 end
