58810907a00a9f3397046a4225e42c851822feff
Raul Ogaz <raul.26_90@hotmail.com>
2012-09-30 01:52:01 -0600

    Se trato de crear compras de productos

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   attr_accessible :product_type_id, :size
   belongs_to :product_type
+  
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,4 @@
 class Purchase < ActiveRecord::Base
   attr_accessible :product_id, :quantity, :unit_price
+  belongs_to :product
 end
